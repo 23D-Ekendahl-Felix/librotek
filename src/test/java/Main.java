90bb5a65cda455a625c31de13cf2c24ae153669f
felix e <felix.ekendahl@elev.ga.ntig.se>
2026-05-04 10:44:01 +0200

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,9 @@
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+import kong.unirest.UnirestException;
+
+import java.lang.reflect.Type;
+import java.util.*;
